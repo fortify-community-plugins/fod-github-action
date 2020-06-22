3d1534b362100fdcf89558978fca3503ee393f79
Kevin Lee <kevin.lee@microfocus.com>
2020-06-22 14:12:27 +0100

    Initial Import

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ try {
     const polling_interval = core.getInput('polling_interval');
 
     console.log(input1);
-    console.log('fod_access_key: ${fod_access_key}!');
-    console.log('fod_secret_key: ${fod_secret_key}!');
-    console.log('bsi_token: ${bsi_token}!');
-    console.log('entitlement_preference: ${entitlement_preference}!');
-    console.log('remediation_scan_preference: ${remediation_scan_preference}!');
-    console.log('in_progress_scan_action: ${in_progress_scan_action}!');
-    console.log('zip_location: ${zip_location}!');
-    console.log('polling_interval: ${polling_interval}!');
+    console.log('fod_access_key: ' + fod_access_key);
+    console.log('fod_secret_key: ' + fod_secret_key);
+    console.log('bsi_token: ' + bsi_token);
+    console.log('entitlement_preference: ' + entitlement_preference);
+    console.log('remediation_scan_preference: ' + remediation_scan_preference);
+    console.log('in_progress_scan_action: ' + in_progress_scan_action'');
+    console.log('zip_location: ' + zip_location);
+    console.log('polling_interval: ' + polling_interval);
 
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log('The event payload: ${payload}');
+    console.log('The event payload: ' + payload);
     
 } catch (error) {
     core.setFailed(error.message);
